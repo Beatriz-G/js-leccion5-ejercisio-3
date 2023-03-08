d7247b577ab45529cf9e136a078a88f875304c2f
beatriz_g <beatrizgue20@gmail.com>
2023-03-08 11:14:01 -0800

    Cambie el boton

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var modal = document.querySelector(".modal");
+var button = document.querySelector(".got-this");
+var modalX = document.querySelector(".modal-x");
+
+button.addEventListener("click", function () {
+    modal.classList.add("show-modal");
+    button.innerText = "You've got this!!!";
+});
+
+modal.addEventListener("click", function() {
+    modal.classList.remove("show-modal");
+    modal.innerTex.add = "Who's got this!";
+});
